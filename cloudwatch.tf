3c0768c1334e5ef783a5a0ab419e4137aa8e5b3d
Simon Cull <simoncull@gmail.com>
2025-01-04 23:51:57 +0000

    Update cloudwatch.tf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_event_rule" "stop_ec2_instances" {
   name        = "stop_ec2_instances"
   description = "Stops running EC2 instances at 18:00"
 
-  schedule_expression = "cron(0 18 * * ? *)" # Runs every day at 18:00]
+  schedule_expression = "cron(0 18 * * MON-FRI *)" # Runs every day at 18:00]
 }
 
 resource "aws_cloudwatch_event_target" "stop_ec2_instances_target" {
@@ -15,7 +15,7 @@ resource "aws_cloudwatch_event_rule" "start_ec2_instances" {
   name        = "start_ec2_instances"
   description = "Starts stopped EC2 instances at 08:00"
 
-  schedule_expression = "cron(0 8 * * ? *)" # Runs every day at 08:00
+  schedule_expression = "cron(0 8 * * MON-FRI *)" # Runs every day at 08:00
 }
 
 resource "aws_cloudwatch_event_target" "start_ec2_instances_target" {
